d9096a0c366ec17dc93fd2c69f68afb3adc3e2af
Yisiang <ongyisiang98@yahoo.com>
2021-09-14 15:35:24 +0800

    minor change at CORS for client side login

--- routes/cors.js
+++ routes/cors.js
@@ -2,18 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://ongyisiang98:3001'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3001'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
     //check whether the header of the origin is in the whitelist using .indexOf
     if(whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true };
+        callback(null,true);
     }
     else {
-        corsOptions = { origin: false };
+        callback(new Error("Not allowed by CORS"))
     }
-    callback(null, corsOptions);
 };
 
 exports.cors = cors();
